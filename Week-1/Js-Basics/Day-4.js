c184afa02a33e559842a3040f65d1701c72bfbae
Lakorthus <julio28ve@gmail.com>
2022-09-29 11:54:02 +0100

    Day 4

--- Week-1/Js-Basics/Day-4.js
+++ Week-1/Js-Basics/Day-4.js
@@ -1,9 +1,27 @@
 // OBJECtTS
-
 let createCat = {
   catBreed: "Stray Cat",
   catColor: "Black",
+  possibleNames: {
+    a: "Fary",
+    b: "Alex",
+    2: "Sunshine",
+    3: "Kitty",
+  },
 };
 
 createCat.catBreed = "Italian";
-console.log(createCat);
+console.table(createCat);
+
+//OBJECT with Fucntion
+// function Book(type, author) {
+//     this.type = type;
+//     this.author = author;
+//     this.getDetails = function () {
+//         return `${this.type} written by ${this.author}`;
+//     }
+// }
+
+// var book = new Book("Woking Hard", "Julio V");
+
+// console.log(book.getDetails());
